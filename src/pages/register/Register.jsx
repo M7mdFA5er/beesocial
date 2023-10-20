f841c498d03a6e7891e524f544f4e2faf65c475d
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2023-10-20 21:50:16 +0300

    Finish Register Page and add react-router to the project

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,29 @@ import './register.scss'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='register'>
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Ea magna qui excepteur quis labore laboris velit mollit proident adipisicing.
+            Veniam duis cupidatat magna et esse esse tempor nisi duis.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder='Username' />
+            <input type="text" placeholder='Email' />
+            <input type="password" placeholder='Password' />
+            <input type="text" placeholder='Name' />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
