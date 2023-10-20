0700dba7ea29a93b00d9adb89c938b42a13e8b26
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2023-10-21 00:32:20 +0300

    setup Router and add Nav-left-right bars as components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import Login from './pages/login/Login'
 import Register from './pages/register/Register';
+import Navbar from './components/navbar/Navbar'
+import LeftBar from './components/leftBar/LeftBar'
+import RightBar from './components/rightBar/RightBar'
+import Home from './pages/home/Home'
+import Profile from './pages/profile/Profile'
+
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
 
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: 'flex' }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+
+
   const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id ",
+          element: <Profile />
+        }
+      ]
+    },
     {
       path: "/login",
       element: <Login />,
